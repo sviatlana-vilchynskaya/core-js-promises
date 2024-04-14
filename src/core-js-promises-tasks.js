71c2c9dd33c60c1d695f22ef7bccf47e676dcac4
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2024-04-14 15:11:21 +0300

    feat: add solutions: getAllResult, queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -143,8 +143,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) =>
+      result.status === 'fulfilled' ? result.value : null
+    )
+  );
 }
 
 /**
@@ -165,8 +169,19 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let resultString = '';
+  let sequence = Promise.resolve();
+
+  promises.forEach((promise) => {
+    sequence = sequence
+      .then(() => promise)
+      .then((value) => {
+        resultString += value.toString();
+      });
+  });
+
+  return sequence.then(() => resultString);
 }
 
 module.exports = {
